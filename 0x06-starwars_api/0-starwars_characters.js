c89fe826fa11eff7d5ede358fe0bdf66491b1096
Jukunye <jukunyes@gmail.com>
2024-05-24 04:57:25 +0300

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const args = process.argv;
+const movieUrlBase = "https://swapi-api.hbtn.io/api/films/";
+// Define URL for movie API endpoint using command-line argument
+const movieUrl = `${movieUrlBase}${args[2]}/`;
+
+let characterIndex = 0;
+let characterUrls = [];
+
+function getMovieDetails() {
+  request(movieUrl, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const movieDetails = JSON.parse(body);
+      characterUrls = movieDetails.characters;
+      if (characterUrls.length > 0) {
+        getCharacterDetails();
+      }
+    } else {
+      console.log(error);
+    }
+  });
+}
+
+// Function to fetch character details from API
+function getCharacterDetails() {
+  if (characterIndex < characterUrls.length) {
+    request(characterUrls[characterIndex], function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const characterDetails = JSON.parse(body);
+        console.log(characterDetails.name);
+        characterIndex++;
+        getCharacterDetails();
+      } else {
+        console.error("error:", error);
+      }
+    });
+  }
+}
+
+getMovieDetails();
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+# 0x06. Star Wars API
+
+This project required me to interact with an external API to fetch and display information about Star Wars characters based on the movie ID provided as argument.
